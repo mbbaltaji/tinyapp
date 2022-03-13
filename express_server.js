9f214795461461fae8162c54096440147099c9a2
Mustapha Baltaji <mbbaltaji@gmail.com>
2022-03-13 19:07:46 +0000

    refacotr: add comments and lint code

--- express_server.js
+++ express_server.js
@@ -19,7 +19,9 @@ app.use(cookieSession({
   maxAge: 24 * 60 * 60 * 1000 // 24 hours
 }));
 
-// Mock url database
+/** USED FOR TESTING **/
+
+// // Mock url database
 const urlDatabase = {
   b2xVn2: {
     longURL: "http://www.lighthouselabs.ca",
@@ -45,11 +47,20 @@ const users = {
   }
 };
 
-//ROUTE HANDLERS
+// const users = {};
+// const urlDatabase = {};
+
+
+
 app.get('/', (req, res) => {
-  res.send('Hello!');
+  if (req.session.user_id) {
+    res.redirect('/urls');
+  } else {
+    res.redirect('/login');
+  }
 });
 
+// displays url inventory of user
 app.get('/urls', (req, res) => {
   const templateVars = {
     user: users[req.session.user_id],
@@ -58,6 +69,7 @@ app.get('/urls', (req, res) => {
   res.render('urls_index', templateVars);
 });
 
+// Creating a new short URL from long URL (if logged in)
 app.get('/urls/new', (req, res) => {
   let session = req.session.user_id;
   
@@ -71,6 +83,7 @@ app.get('/urls/new', (req, res) => {
   }
 });
 
+//
 app.get('/urls/:shortURL', (req, res) => {
   if (urlDatabase[req.params.shortURL]) {
     let session = req.session.user_id;
@@ -84,6 +97,34 @@ app.get('/urls/:shortURL', (req, res) => {
   } else {
     res.status(404).send('The short URL entered does not exist');
   }
+});
+
+app.get('/u/:shortURL', (req, res) => {
+  let validUrlId = urlDatabase[req.params.shortURL];
+  if (validUrlId) {
+    const longURL = urlDatabase[req.params.shortURL]['longURL'];
+    res.redirect(longURL);
+  } else {
+    return res.status(404).send('Page NOT found. Wrong id in /u/:id');
+  }
+  
+});
+
+app.get('/register', (req, res) => {
+  let templateVars = {
+    user: users[req.session.user_id],
+    error: {}
+  };
+  res.render('registration', templateVars);
+});
+
+app.get('/login', (req, res) => {
+  let templateVars = {
+    error: {},
+    user: users[req.session.user_id]
+  };
+  res.render('login', templateVars);
+  
 });
 
 app.post('/urls', (req, res) => {
@@ -100,15 +141,10 @@ app.post('/urls', (req, res) => {
   }
 });
 
-app.get('/u/:shortURL', (req, res) => {
-  let validUrlId = urlDatabase[req.params.shortURL];
-  if (validUrlId) {
-    const longURL = urlDatabase[req.params.shortURL]['longURL'];
-    res.redirect(longURL);
-  } else {
-    return res.status(404).send('Page NOT found. Wrong id in /u/:id');
-  }
-  
+app.post('/urls/:shortURL', (req, res) => {
+  const shortURL = req.params.shortURL;
+  urlDatabase[shortURL]['longURL'] = req.body.longURL;
+  res.redirect('/urls');
 });
 
 app.post('/urls/:shortURL/delete', (req, res) => {
@@ -124,25 +160,6 @@ app.post('/urls/:shortURL/delete', (req, res) => {
   res.status(401).send('You are unauthorized to delete this url');
 });
 
-app.post('/urls/:shortURL', (req, res) => {
-  const shortURL = req.params.shortURL;
-  urlDatabase[shortURL]['longURL'] = req.body.longURL;
-  res.redirect('/urls');
-});
-
-app.post('/logout', (req, res) => {
-  req.session = null;
-  res.redirect('/urls');
-});
-
-app.get('/register', (req, res) => {
-  let templateVars = {
-    user: users[req.session.user_id],
-    error: {}
-  }
-  res.render('registration', templateVars);
-});
-
 app.post('/register', (req, res) => {
   let email = req.body.email;
   let password = req.body.password;
@@ -153,7 +170,7 @@ app.post('/register', (req, res) => {
     templateVars = {
       user: users[req.session.user_id],
       error: { msg: 'Missing Email or Password' }
-    }
+    };
     res.status(400);
     return res.render('registration', templateVars);
   }
@@ -162,7 +179,7 @@ app.post('/register', (req, res) => {
     templateVars = {
       user: users[req.session.user_id],
       error: { msg: 'Email already exists!' }
-    }
+    };
     res.status(400);
     return res.render('registration', templateVars);
   }
@@ -177,33 +194,28 @@ app.post('/register', (req, res) => {
   res.redirect('/urls');
 });
 
-app.get('/login', (req, res) => {
-  let templateVars = {
-    error: {},
-    user: users[req.session.user_id]
-  }
-  res.render('login', templateVars);
-  
-});
-
 app.post('/login', (req, res) => {
   let email = req.body.email;
   let password = req.body.password;
   let id = getUserByEmail(email, users);
   let templateVars = {};
 
-
   if (!isValidEmail(email, users) || !bcrypt.compareSync(password, users[id].password)) {
     templateVars = {
       user: users[req.session.user_id],
       error: {
         msg: 'Invalid email or password!',
-      } 
-  }
-  res.status(400);
-  return res.render('login', templateVars);
+      }
+    };
+    res.status(400);
+    return res.render('login', templateVars);
   }
   req.session.user_id = id;
+  res.redirect('/urls');
+});
+
+app.post('/logout', (req, res) => {
+  req.session = null;
   res.redirect('/urls');
 });
 
